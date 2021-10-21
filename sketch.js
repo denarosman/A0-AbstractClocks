3adf8226a65dc39b64459680ae067644caab596b
Dena Rosman <denaprosman@gmail.com>
2021-10-21 14:02:37 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,46 @@
-function setup(){
-	createCanvas(800, 600); // make an HTML canvas element width x height pixels
-	stroke(255);
-	//console.log(minute());
-	minutePrint = minute();
+function setup() {
+    createCanvas(windowWidth, windowHeight); 
+	c1 = color(226, 197, 191);
+	c2 = color(255, 255, 255);
+	c3 = color(237, 228, 222, 255); 
+	background(c1);
 }
 
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+	background(c1);
+	draw();
+ }
+
 function draw() {
-	let wheel = ('white');
-	background("#E0C5BE");
+	background(c1);
 	strokeWeight(0);
-
-	if  (minutePrint != minute())
-	{
-		console.log(minute());
-		minutePrint = minute();
-	}
-
-	var h = hour() * (60/24) * PI / 180;
-	var m = minute() * PI / 180;
-	var s = second() * PI / 180;
 
 	let _60Deg = 60 * PI / 180;
 	let _360Deg = 360 * PI / 180;
 
-	var hAngle = map(h, 0, _60Deg, 0, _360Deg);
-	var mAngle = map(m, 0, _60Deg, 0, _360Deg);
-	var sAngle = map(s, 0, _60Deg, 0, _360Deg);
+	let xPos = windowWidth/2;
+	let yPos = windowHeight/2;
 
-	let x = 400;
-	let y = 300;
-	let constant = 0.9
+	let constant = .9;
 	let size_arc = 250 * constant;
 	let size_c = 140 * constant;
-	
-	fill(237, 228, 222, 255); circle(x - 265,  y, size_c);  
-	fill(wheel); arc(x - 265, y, size_arc, size_arc, 0, hAngle, OPEN);
 
-	fill(237, 228, 222, 255); circle(x,  y, size_c);  
-	fill(wheel); arc(x, y, size_arc, size_arc, 0, mAngle, OPEN);
+	let h = [-1, hour()/24];
+	let m = [0, minute()/60];
+	let s = [1, second()/60];
 
-	fill(237, 228, 222, 255); circle(x + 265,  y, size_c);  
-	fill(wheel); arc(x + 265, y, size_arc, size_arc, 0, sAngle, OPEN);
-	
+	let units = [h, m, s]
+
+	for (let i = 0; i < units.length; i++) {
+
+		x = xPos + units[i][0] * 265;
+		y = yPos;
+
+		angle = map(units[i][1]/3 * PI, 0, _60Deg, 0, _360Deg);
+
+		fill(c3); circle(x, y, size_c);  
+		fill(c2); arc(x, y, size_arc, size_arc, 0, angle, OPEN);
+	  }	
 	endShape();
 }
